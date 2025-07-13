b804fbf22c5bb648d6b4732ebc08479435b75185
Pablo Quemé <pgqueme@gmail.com>
2025-07-13 15:07:26 -0600

    Archivos base

---
+++ faser-prueba-tecnica-v2/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AddTaskDialogComponent } from './components/add-task-dialog/add-task-dialog.component';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule, AddTaskDialogComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Control de Tareas';
+  tasks: Task[] = [];
+  showDialog = false;
+  selectedTask: Task | null = null;
+
+  addTask() {
+    this.showDialog = true;
+  }
+
+  onTaskAdded(newTask: Task) {
+    this.tasks.push(newTask);
+    this.showDialog = false;
+  }
+
+  onDialogClosed() {
+    this.showDialog = false;
+  }
+
+  selectTask(task: Task) {
+    this.selectedTask = task;
+  }
+
+  deleteTask() {
+    if (this.selectedTask) {
+      this.tasks = this.tasks.filter(task => task.id !== this.selectedTask!.id);
+      this.selectedTask = null;
+    }
+  }
+
+  isTaskSelected(task: Task): boolean {
+    return this.selectedTask?.id === task.id;
+  }
+}
